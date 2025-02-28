516c462af109a7c75a193c1f8876b17c41e12b5b
Daniil <daniil.shishaev@gmail.com>
2025-02-28 17:16:11 +0300

    5hw done

--- src/s2-homeworks/hw04/common/c2-SuperButton/SuperButton.tsx
+++ src/s2-homeworks/hw04/common/c2-SuperButton/SuperButton.tsx
@@ -25,7 +25,6 @@ const SuperButton: React.FC<SuperButtonPropsType> = (
                 : xType === 'secondary'
                     ? ' ' + s.secondary : ' ' + s.default)
         + (className ? ' ' + className : '') // задачка на смешивание классов
-    console.log(finalClassName)
     return (
         <button
             disabled={disabled}
