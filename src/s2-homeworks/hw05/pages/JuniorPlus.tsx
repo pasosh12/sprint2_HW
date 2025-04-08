7e9ca84f54149bfef28fba76875ecd6572986248
Daniil <daniil.shishaev@gmail.com>
2025-04-08 15:46:39 +0300

    HW 10 done

--- src/s2-homeworks/hw05/pages/JuniorPlus.tsx
+++ src/s2-homeworks/hw05/pages/JuniorPlus.tsx
@@ -1,5 +1,8 @@
 import React from 'react'
-// import HW10 from '../../hw10/HW10'
+import HW10 from '../../hw10/HW10'
+import {Provider} from "react-redux";
+import store from "../../hw10/bll/store";
+import HW11 from "../../hw11/HW11";
 // import HW11 from '../../hw11/HW11'
 // import HW12 from '../../hw12/HW12'
 // import HW13 from '../../hw13/HW13'
@@ -8,14 +11,16 @@ import React from 'react'
 
 function JuniorPlus() {
     return (
+        // <Provider store={store}>
         <div id={'hw5-page-junior-plus'}>
-            {/*<HW10 />*/}
-            {/*<HW11 />*/}
+            <HW10 />
+            <HW11 />
             {/*<HW12 />*/}
             {/*<HW13 />*/}
             {/*<HW14 />*/}
             {/*<HW15 />*/}
         </div>
+        // </Provider>
     )
 }
 
--- src/s2-homeworks/hw10/HW10.tsx
+++ src/s2-homeworks/hw10/HW10.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import {useDispatch, useSelector} from 'react-redux'
-import {AppStoreType} from './bll/store'
+import store, {AppStoreType} from './bll/store'
 import {loadingAC} from './bll/loadingReducer'
 import SuperButton from '../hw04/common/c2-SuperButton/SuperButton'
 import s2 from '../../s1-main/App.module.css'
@@ -14,13 +14,16 @@ import {Loader} from './Loader'
 * */
 
 const HW10 = () => {
-    // useSelector, useDispatch // пишет студент
-    const isLoading = false
+    const isLoading = useSelector<AppStoreType>(state => state.loading.isLoading)
 
+    const dispatch = useDispatch()
     const setLoading = () => { // пишет студент // показать крутилку на 1,5 секунд
-        // dispatch
 
-        // setTimeout
+        dispatch(loadingAC(true))
+        const loadingTimeout = setTimeout(() => {
+            dispatch(loadingAC(false))
+        }, 1500)
+        // clearTimeout(loadingTimeout)
     }
 
     return (
